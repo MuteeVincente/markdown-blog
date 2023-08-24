9b9dafc4af8eec75f0cd9dd9301f59ec14648786
MuteeVincente <vinniemutee@gmail.com>
2023-08-25 00:24:19 +0300

    Date String correction

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use('/articles', articleRouter);
 app.get('/', (req, res) => {
     const articles = [{
         title:'Test Article',
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description:'Test Description'
 
     },
     {
         title:'Test Article 2',
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description:'Test Description 2'
 
     }
