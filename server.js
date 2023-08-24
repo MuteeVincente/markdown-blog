7b43346f9c1faa7a205e5b08b20e0b537870c38e
MuteeVincente <vinniemutee@gmail.com>
2023-08-25 00:00:25 +0300

    Templating and Bootsrapping the code

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const articleRouter = require('./routes/articles');
 
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [{
+        title:'Test Article',
+        createdAt: Date.now(),
+        description:'Test Description'
+
+    },
+    {
+        title:'Test Article 2',
+        createdAt: Date.now(),
+        description:'Test Description 2'
+
+    }
+]
+
+    res.render('index', {articles: articles});
 })
 
 app.listen(5000);
